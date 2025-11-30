d506737f87f15010864e8a41e65f5ea1a9f659ed
Michael Welter <me@mikinho.com>
2025-11-30 11:53:07 -0600

    1.0.6

--- dist/active-links.js
+++ dist/active-links.js
@@ -4,14 +4,14 @@
  *
  * @copyright 2025 Michael Welter <me@mikinho.com>
  * @license MIT
- * @version 1.0.5
+ * @version 1.0.6
  */
 
 (function () {
     "use strict";
 
     // This string will be replaced by the GitHub Action
-    const VERSION = "1.0.5";
+    const VERSION = "1.0.6";
 
     const CONFIG = {
         version: VERSION, 
